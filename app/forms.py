30ed25207f5e49898ee1812f298ab930e061531b
Govind Joshi <joshi.govind12@gmail.com>
2020-01-23 21:38:31 -0600

    Added avatars to profiles and posts, along with updated user page

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
@@ -27,3 +27,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('This email has already been taken...')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=1, max=140)])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 maintenance_mode = False
 
@@ -17,6 +18,18 @@ def check_for_maintenance():
 @app.route('/maintenance')
 def maintenance():
     return render_template('maintenance.html'), 503
+
+
+# No need to call db.session.add(user) since
+# when current_user is referenced, flask-login
+# will invoke the user loader callback function
+# which will run a database query that will
+# put the target user in the database session
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
@@ -42,10 +55,29 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
-        {'author:': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+# Check request.method for different cases
+# If initial GET request by browser, populate fields with current db info
+# else in case of a invalid POST request, re-render field without changing data
+@app.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved!")
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
